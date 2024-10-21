f5003c7ea1e83c999aa69444407537afbedb3d03
Nvf23 <nicovfp23@gmail.com>
2024-10-21 19:16:26 +0200

    main

---
+++ prueba/back_end/src/main/java/com/sepa/back_end/entities/Transaction.java
@@ -0,0 +1,55 @@
+package com.sepa.back_end.entities;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    Long id;
+
+    @Getter @Setter
+    String nameCompany;
+
+    @Getter @Setter
+    String ibanCompany;
+
+    @Getter @Setter
+    String bicCompany;
+
+    @Getter @Setter
+    String nameClient;
+
+    @Getter @Setter
+    String ibanClient;
+
+    @Getter @Setter
+    String bicClient;
+
+    @Getter @Setter
+    String amount;
+
+    @Getter @Setter
+    String date;
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", name company=" + nameCompany +
+                ", iban company=" + ibanCompany +
+                ", bic company=" + bicCompany +
+                ", name client=" + nameClient +
+                ", iban client=" + ibanClient +
+                ", bic client=" + bicClient +
+                ", cantidad=" + amount +'€' +
+                ", fecha='" + date + '\'';
+    }
+}
