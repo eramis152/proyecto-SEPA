f0c6082c977d3c46e2423c76c291ca3195fc8d6f
Nico <140804627+Nvf23@users.noreply.github.com>
2024-10-29 17:37:00 +0100

    modificacion

---
+++ target/classes/com/sepa/front_end/src/app/services/transaction.service.ts
@@ -0,0 +1,23 @@
+import { transaction } from './../../model/transaction';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TransactionService {
+
+
+  private url: string =  'http://localhost:8080/transaction';  // URL base
+
+  constructor(private http: HttpClient) { }
+
+  emitTransaction(transaction: transaction): Observable<transaction>{
+
+    return this.http.post<transaction>(this.url,transaction);
+
+  }
+
+}
